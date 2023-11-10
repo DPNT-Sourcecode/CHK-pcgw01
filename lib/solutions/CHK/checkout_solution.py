9c0527569bac763871529cc9baf917d8497dc36b
sbx_user1051 <sbx_user1051@169.254.23.205>
2023-11-10 14:46:43 +0000

    Fri Nov 10 14:46:43 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,28 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    pricing_table = {
+        "A": {
+            1: 50,
+            3: 130
+        },
+        "B": {
+            1: 30,
+            2: 45
+        },
+        "C": {
+            1: 20
+        },
+        "D": {
+            1: 15
+        }
+    }
     
+    current_basket = {
+        "A": 0,
+        "B": 0,
+        "C": 0,
+        "D": 0,
+    }
+
 
