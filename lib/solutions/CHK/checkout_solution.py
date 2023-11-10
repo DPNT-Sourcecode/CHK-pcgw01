4632446ae5e68054ba07cd2bbb0961589616defb
sbx_user1051 <sbx_user1051@169.254.23.205>
2023-11-10 15:26:30 +0000

    Fri Nov 10 15:26:30 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,7 @@ def get_pricing_table():
         "A": {
             "one": 50,
             "triple": 130,
-            "penta": 200
+            "quintuple": 200
         },
         "B": {
             "one": 30,
@@ -18,10 +18,9 @@ def get_pricing_table():
         },
         "D": {
             "one": 15
-        }
+        },
         "E": {
-            "one": 40,
-            "double": 80
+            "one": 40
         }
     }
 
@@ -42,21 +41,33 @@ def checkout(skus):
         "B": skus.count("B"),
         "C": skus.count("C"),
         "D": skus.count("D"),
+        "E": skus.count("E"),
     }
 
     basket_value = 0
 
     for item, value in current_basket.items():
         if item == "A":
-            basket_value += (value // 3) * pricing_table[item]["triple"]
-            basket_value += (value % 3) * pricing_table[item]["one"]
+            basket_value += value // 5 * pricing_table[item]["quintuple"]
+            quintuple_leftover = value % 5
+            basket_value += (quintuple_leftover // 3) * pricing_table[item]["triple"]
+            triple_leftover = quintuple_leftover % 3
+            basket_value += triple_leftover * pricing_table[item]["one"]
         elif item == "B":
             basket_value += (value // 2) * pricing_table[item]["double"]
             basket_value += (value % 2) * pricing_table[item]["one"]
+        elif item == "E":
+            price_deals = value // 2
+            basket_value += price_deals * 2 * pricing_table[item]["one"]
+            price_removal = (value % 2) - price_deals
+            if price_removal < 0:
+                price_removal = 0
+            basket_value += price_removal * pricing_table[item]["one"]
         else:
             basket_value += value * pricing_table[item]["one"]
 
     return basket_value
+
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -5,5 +5,10 @@ def test_valid_checkout():
     assert checkout("CABABAD") == 210
 
 
+def test_secondary_valid_checkout():
+    assert checkout("CABABAD") == 210
+
+
 def test_invalid_checkout():
     assert checkout("TESTINGCHECKOUT") == -1
+
